bb7a94a8596068fad24c84ddf76449dd06989a3d
DrBr4n <drbr4h@hotmail.com>
2021-05-23 22:25:19 +0100

    502.c

--- 502.c
+++ 502.c
@@ -82,6 +82,175 @@ LInt reverseL (LInt l){
 //    *l = head;
 //}
 
+//5
+void insertOrd (LInt *l, int x){
+
+    LInt new = malloc(sizeof(struct lligada));
+    new->valor = x;
+
+    LInt *cur = l;
+    while (*cur && (*cur)->valor < x) {
+        cur = &((*cur)->prox);
+    }
+    new->prox = *cur;
+    *cur = new;
+}
+
+//6
+int removeOneOrd (LInt *l, int x){
+    
+    int r = 1;
+    LInt *cur = l;
+
+    while (*cur && (*cur)->valor != x) {
+        cur = &((*cur)->prox);
+    }
+    if (*cur) {
+        r = 0;
+        LInt del = *cur;
+        *cur = (*cur)->prox;
+        free(del);
+    }
+    return r;
+}
+
+//9
+LInt parteAmeio (LInt *l){
+    int n = 0;
+    int i = 0;
+    LInt *c = l;
+    LInt new_head = NULL;
+    LInt curr = NULL;
+
+    while (*c){
+        n++;
+        c = &((*c)->prox);
+    }
+
+    while (*l && i < n/2) {
+        if(!new_head) new_head = curr = *l;
+        else curr = *l;
+        *l = (*l)->prox;
+        i++;
+    }
+    curr->prox = NULL;
+
+    return new_head;
+}
+
+//10
+int removeAll (LInt *l, int x){
+    int c = 0;
+    while (*l) {
+        if ((*l)->valor == x) {
+            *l = (*l)->prox;
+            c++;
+        }
+        else l = &((*l)->prox);
+    }
+    
+    return c;
+}
+
+//12
+int removeMaiorL (LInt *l){
+    
+    LInt *cur = l;
+    LInt *ptr = l;
+    int max = (*cur)->valor;
+
+    while (*cur) {
+        if ((*cur)->valor > max) max = (*cur)->valor;
+        cur = &((*cur)->prox);
+    }
+
+    while (*l && (*l)->valor != max) {
+        l  = &((*l)->prox);
+    }
+    
+    LInt tmp = *l; 
+    *l = (*l)->prox;
+    free(tmp);
+
+
+    return max;
+}
+
+//13
+void init (LInt *l){
+    LInt prev = NULL;
+
+    while ((*l)->prox) {
+        prev = *l;
+        l = &((*l)->prox);
+    }
+    free(*l);
+    if(prev) prev->prox = NULL;
+    else *l = NULL;
+}
+
+//14
+void appendL (LInt *l, int x){
+
+    LInt new = malloc(sizeof(struct lligada));
+    new->valor = x;
+    new->prox = NULL;
+
+    if(!(*l)) *l = new; 
+    
+    else {
+        while (*l && (*l)->prox) {
+            l = &((*l)->prox);
+        }
+        (*l)->prox = new;
+    }
+}
+
+//15
+void concatL (LInt *a, LInt b){
+
+    if(!(*a)) *a = b; 
+    else{
+        while ((*a)) {
+            a = &((*a)->prox);
+        }
+        
+        *a = b;
+    }
+}
+
+//16
+LInt cloneL (LInt l) {
+    LInt new_head = NULL;
+    LInt curr = NULL;
+    while(l) {
+        LInt new = malloc(sizeof(struct lligada));
+        new->valor = l->valor;
+        new->prox = NULL;
+
+        if(new_head == NULL) new_head = curr = new;
+        else {
+            curr->prox = new;
+            curr = new; 
+        }
+        l = l->prox;
+    }
+    return new_head;
+}
+
+//17
+LInt cloneRev (LInt l) {
+    LInt new_head = NULL;
+    while(l) {
+        LInt new = malloc(sizeof(struct lligada));
+        new->valor = l->valor;
+        new->prox = new_head;
+        new_head = new;
+        l = l->prox;
+    }
+    return new_head;
+}
+
 //18
 int maximo (LInt l){
     
@@ -94,6 +263,29 @@ int maximo (LInt l){
     }
 
     return max;
+}
+
+//19
+int take (int n, LInt *l){
+    int i = 0;
+    for (i = 0; *l && n > 0 ; i++, n--, l = &((*l)->prox));
+    free(*l);
+    *l = NULL;
+    return i;
+}
+
+//20
+int drop (int n, LInt *l){
+    int c = 0;
+    while (*l && n > 0) {
+        LInt tmp = *l;
+        n--;
+        c++;
+        *l = (*l)->prox;
+        free(tmp);
+    }
+   
+    return c;
 }
 
 //21
@@ -166,3 +358,87 @@ LInt somasAcL (LInt l) {
     return head;
 }
 
+//25
+void remreps (LInt l){
+    if (l != NULL) {
+        while (l->prox != NULL) {
+            if (l->prox->valor == l->valor) {
+                LInt tmp = l->prox;
+                l->prox = tmp->prox;
+                free(tmp);
+            }else l = l->prox;
+        }
+    }
+}
+
+//26
+LInt rotateL (LInt l){
+    
+    if (l == NULL || l->prox == NULL) return l;
+    
+    LInt fst = l;
+    LInt snd = l->prox;
+
+    while (l->prox != NULL) l = l->prox;
+    
+    fst->prox = NULL;
+    l->prox = fst;
+
+    return snd;
+}
+
+//27
+LInt parte (LInt l){
+    
+    LInt new_head, tmp;
+    new_head = temp = l->prox;
+
+    while(l && l->prox) {
+
+        tmp->prox = l->prox;
+        tmp = l->prox;
+
+        l->prox = l->prox->prox;
+        l = l->prox;
+
+        temp->prox = NULL;
+    }
+    return new_head;
+}
+typedef struct nodo {
+	int valor;
+	struct nodo *esq, *dir;
+} *ABin;
+
+//28
+int altura (ABin a){
+
+    if (a == NULL) return 0;
+
+	return 1 + (altura(a->esq) > altura(a->dir) ? altura(a->esq) : altura(a->dir));
+}
+
+//29
+ABin cloneAB (ABin a) {
+    if (!a) return NULL;
+    
+    ABin res = malloc(sizeof(struct nodo));
+    res->valor = a->valor;
+    res->esq = cloneAB(a->esq);
+    res->dir = cloneAB(a->dir);
+
+    return res;
+}
+
+//30
+void mirror (ABin *a) {
+    if (*a) {
+        ABin tmp = (*a)->esq;
+        (*a)->esq = (*a)->dir;
+        (*a)->dir = tmp;
+        mirror(&((*a)->esq));
+        mirror(&((*a)->dir));
+    }
+}
+
+ABin a = newABin(2, newABin(4, newABin(6, NULL, newABin(8, NULL, NULL)), NULL), newABin(10, NULL, newABin(22, newABin(25, NULL, NULL), NULL)));
